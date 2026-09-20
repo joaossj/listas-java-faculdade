9ec032d46238f82d807d99df2d5bcc4ca4a1a644
joaossj <164758879+joaossj@users.noreply.github.com>
2026-09-20 15:43:22 -0400

    Update lista03.java

--- lista03/lista03.java
+++ lista03/lista03.java
@@ -5,191 +5,257 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        
-        System.out.println("Qual exercicio quer rodar? (1 a 10):");
+
+        System.out.print("Escolha o exercicio (1 a 10): ");
         int ex = ler.nextInt();
-        ler.nextLine();
-        
+
         switch (ex) {
-            case 1:
-                System.out.print("Digite N: ");
-                int n1 = ler.nextInt();
-                if (n1 < 1) {
-                    System.out.println("não há valores a exibir");
-                } else {
-                    int i = 1;
-                    while (i <= n1) {
-                        System.out.println(i);
-                        i++;
+
+        case 1:
+            System.out.print("Digite N: ");
+            int n = ler.nextInt();
+
+            if (n < 1) {
+                System.out.println("Nao ha valores");
+            } else {
+                int i = 1;
+
+                while (i <= n) {
+                    System.out.println(i);
+                    i++;
+                }
+            }
+            break;
+
+        case 2:
+            System.out.print("Digite um numero: ");
+            int num = ler.nextInt();
+
+            int i = 1;
+
+            while (i <= 10) {
+                System.out.println(num + " x " + i + " = " + (num * i));
+                i++;
+            }
+            break;
+
+        case 3:
+            double nota;
+
+            do {
+                System.out.print("Digite a nota (0 a 10): ");
+                nota = ler.nextDouble();
+
+                if (nota < 0 || nota > 10)
+                    System.out.println("Nota invalida");
+
+            } while (nota < 0 || nota > 10);
+
+            System.out.println("Nota aceita: " + nota);
+            break;
+
+        case 4:
+            int op;
+
+            do {
+                System.out.println("1 - Dobro");
+                System.out.println("2 - Metade");
+                System.out.println("3 - Quadrado");
+                System.out.println("0 - Sair");
+                System.out.print("Opcao: ");
+                op = ler.nextInt();
+
+                if (op >= 1 && op <= 3) {
+                    System.out.print("Digite um numero: ");
+                    double x = ler.nextDouble();
+
+                    if (op == 1)
+                        System.out.println("Dobro: " + (x * 2));
+                    else if (op == 2)
+                        System.out.println("Metade: " + (x / 2));
+                    else
+                        System.out.println("Quadrado: " + (x * x));
+
+                } else if (op != 0) {
+                    System.out.println("Opcao invalida");
+                }
+
+            } while (op != 0);
+            break;
+
+        case 5:
+            System.out.print("Digite A: ");
+            int a = ler.nextInt();
+
+            System.out.print("Digite B: ");
+            int b = ler.nextInt();
+
+            if (a > b) {
+                System.out.println("Intervalo invalido");
+            } else {
+                int soma = 0;
+
+                for (int i = a; i <= b; i++) {
+                    if (i % 2 == 0)
+                        soma = soma + i;
+                }
+
+                System.out.println("Soma dos pares: " + soma);
+            }
+            break;
+
+        case 6:
+            System.out.print("Digite um numero: ");
+            int fat = ler.nextInt();
+
+            if (fat < 0) {
+                System.out.println("Numero invalido");
+            } else {
+                long resultado = 1;
+
+                for (int i = 1; i <= fat; i++) {
+                    resultado = resultado * i;
+                }
+
+                System.out.println("Fatorial: " + resultado);
+            }
+            break;
+
+        case 7:
+            System.out.print("Quantidade de alunos: ");
+            int qtd = ler.nextInt();
+
+            double soma = 0;
+            int maior = 0;
+            int menor = 0;
+
+            for (int i = 1; i <= qtd; i++) {
+                System.out.print("Nota do aluno " + i + ": ");
+                double notaAluno = ler.nextDouble();
+
+                soma = soma + notaAluno;
+
+                if (notaAluno >= 6)
+                    maior++;
+                else
+                    menor++;
+            }
+
+            double media = 0;
+
+            if (qtd > 0)
+                media = soma / qtd;
+
+            System.out.printf("Media da turma: %.2f\n", media);
+            System.out.println("Notas maiores ou iguais a 6: " + maior);
+            System.out.println("Notas abaixo de 6: " + menor);
+            break;
+
+        case 8:
+            int qtdValores = 0;
+            int somaValores = 0;
+            int maiorValor = 0;
+            int menorValor = 0;
+
+            while (true) {
+                System.out.print("Digite um valor (0 para sair): ");
+                int valor = ler.nextInt();
+
+                if (valor == 0)
+                    break;
+
+                if (qtdValores == 0) {
+                    maiorValor = valor;
+                    menorValor = valor;
+                }
+
+                qtdValores++;
+                somaValores = somaValores + valor;
+
+                if (valor > maiorValor)
+                    maiorValor = valor;
+
+                if (valor < menorValor)
+                    menorValor = valor;
+            }
+
+            if (qtdValores == 0) {
+                System.out.println("Nenhum valor foi lido");
+            } else {
+                System.out.println("Quantidade: " + qtdValores);
+                System.out.println("Soma: " + somaValores);
+                System.out.println("Maior: " + maiorValor);
+                System.out.println("Menor: " + menorValor);
+            }
+            break;
+
+        case 9:
+            System.out.print("Digite N (1 a 9): ");
+            int n9 = ler.nextInt();
+
+            if (n9 < 1 || n9 > 9) {
+                System.out.println("Numero invalido");
+            } else {
+
+                for (int i = 1; i <= n9; i++) {
+
+                    for (int j = 1; j <= 10; j++) {
+                        System.out.println(i + " x " + j + " = " + (i * j));
                     }
-                }
-                break;
 
-            case 2:
-                System.out.print("Digite um numero: ");
-                int numTab = ler.nextInt();
-                int j = 1;
-                while (j <= 10) {
-                    System.out.println(numTab + " x " + j + " = " + (numTab * j));
-                    j++;
+                    System.out.println();
                 }
-                break;
+            }
+            break;
 
-            case 3:
-                double nota;
-                do {
-                    System.out.print("Digite a nota (0 a 10): ");
-                    nota = ler.nextDouble();
-                    if (nota < 0 || nota > 10) {
-                        System.out.println("valor inválido");
-                    }
-                } while (nota < 0 || nota > 10);
-                System.out.println("Nota aceita: " + nota);
-                break;
+        case 10:
+            double total = 0;
+            int itens = 0;
 
-            case 4:
-                int opMenu;
-                do {
-                    System.out.println("1 - Dobro\n2 - Metade\n3 - Quadrado\n0 - Sair");
-                    System.out.print("Opção: ");
-                    opMenu = ler.nextInt();
-                    
-                    if (opMenu >= 1 && opMenu <= 3) {
-                        System.out.print("Digite o numero: ");
-                        double numOp = ler.nextDouble();
-                        switch (opMenu) {
-                            case 1 -> System.out.println("Dobro: " + (numOp * 2));
-                            case 2 -> System.out.println("Metade: " + (numOp / 2));
-                            case 3 -> System.out.println("Quadrado: " + (numOp * numOp));
-                        }
-                    } else if (opMenu != 0) {
-                        System.out.println("Opção inválida");
-                    }
-                } while (opMenu != 0);
-                break;
+            while (true) {
+                System.out.print("Codigo do produto (0 para sair): ");
+                int codigo = ler.nextInt();
 
-            case 5:
-                System.out.print("Digite A: ");
-                int a = ler.nextInt();
-                System.out.print("Digite B: ");
-                int b = ler.nextInt();
-                if (a > b) {
-                    System.out.println("intervalo é inválido");
-                } else {
-                    int somaPares = 0;
-                    for (int k = a; k <= b; k++) {
-                        if (k % 2 == 0) {
-                            somaPares += k;
-                        }
-                    }
-                    System.out.println("Soma dos pares: " + somaPares);
-                }
-                break;
+                if (codigo == 0)
+                    break;
 
-            case 6:
-                System.out.print("Digite um numero: ");
-                int numFat = ler.nextInt();
-                if (numFat < 0) {
-                    System.out.println("entrada inválida");
-                } else {
-                    long fat = 1;
-                    for (int k = 1; k <= numFat; k++) {
-                        fat *= k;
-                    }
-                    System.out.println("Fatorial: " + fat);
-                }
-                break;
+                System.out.print("Preco: ");
+                double preco = ler.nextDouble();
 
-            case 7:
-                System.out.print("Quantidade de alunos: ");
-                int qtdAlunos = ler.nextInt();
-                double somaNotas = 0;
-                int maior6 = 0, menor6 = 0;
-                for (int k = 1; k <= qtdAlunos; k++) {
-                    System.out.print("Nota do aluno " + k + ": ");
-                    double nAlu = ler.nextDouble();
-                    somaNotas += nAlu;
-                    if (nAlu >= 6.0) maior6++;
-                    else menor6++;
-                }
-                double medTurma = (qtdAlunos > 0) ? (somaNotas / qtdAlunos) : 0;
-                System.out.printf("Média da turma: %.2f\n", medTurma);
-                System.out.println("Alunos maiores ou iguais a 6: " + maior6);
-                System.out.println("Alunos abaixo de 6: " + menor6);
-                break;
+                System.out.print("Quantidade: ");
+                int quantidade = ler.nextInt();
 
-            case 8:
-                int qtdValores = 0, somaValores = 0;
-                int maiorVal = Integer.MIN_VALUE, menorVal = Integer.MAX_VALUE;
-                boolean primeiro = true;
-                while (true) {
-                    System.out.print("Digite um valor (0 para sair): ");
-                    int val = ler.nextInt();
-                    if (val == 0) {
-                        if (primeiro) System.out.println("nenhum valor foi lido");
-                        break;
-                    }
-                    primeiro = false;
-                    qtdValores++;
-                    somaValores += val;
-                    if (val > maiorVal) maiorVal = val;
-                    if (val < menorVal) menorVal = val;
-                }
-                if (qtdValores > 0) {
-                    System.out.println("Quantidade lida: " + qtdValores);
-                    System.out.println("Soma: " + somaValores);
-                    System.out.println("Maior: " + maiorVal);
-                    System.out.println("Menor: " + menorVal);
-                }
-                break;
+                total = total + (preco * quantidade);
+                itens = itens + quantidade;
+            }
 
-            case 9:
-                System.out.print("Digite N (1 a 9): ");
-                int n9 = ler.nextInt();
-                if (n9 < 1 || n9 > 9) {
-                    System.out.println("entrada inválida");
-                } else {
-                    for (int m = 1; m <= n9; m++) {
-                        for (int o = 1; o <= 10; o++) {
-                            System.out.println(m + " x " + o + " = " + (m * o));
-                        }
-                        System.out.println();
-                    }
-                }
-                break;
+            System.out.println("1 - Dinheiro");
+            System.out.println("2 - Debito");
+            System.out.println("3 - Credito");
+            System.out.print("Forma de pagamento: ");
+            int pagamento = ler.nextInt();
 
-            case 10:
-                double totCompra = 0;
-                int totItens = 0;
-                while (true) {
-                    System.out.print("Código do produto (0 para sair): ");
-                    int cod = ler.nextInt();
-                    if (cod == 0) break;
-                    System.out.print("Preço unitário: ");
-                    double preco = ler.nextDouble();
-                    System.out.print("Quantidade: ");
-                    int qItens = ler.nextInt();
-                    totCompra += (preco * qItens);
-                    totItens += qItens;
-                }
-                System.out.println("Forma de pagamento (1-Dinheiro, 2-Débito, 3-Crédito): ");
-                int fPag = ler.nextInt();
-                switch (fPag) {
-                    case 1 -> totCompra *= 0.95;
-                    case 3 -> totCompra *= 1.03;
-                    case 2 -> {}
-                    default -> System.out.println("Forma inválida");
-                }
-                if (totCompra > 300.00) System.out.println("Aviso: compra grande");
-                if (totItens > 20) System.out.println("Aviso: muitos itens");
-                System.out.printf("Total final: R$ %.2f\n", totCompra);
-                break;
+            if (pagamento == 1)
+                total = total * 0.95;
+            else if (pagamento == 3)
+                total = total * 1.03;
+            else if (pagamento != 2)
+                System.out.println("Forma invalida");
 
-            default:
-                System.out.println("Opção inválida! Escolha de 1 a 10.");
-                break;
+            if (total > 300)
+                System.out.println("Aviso: compra grande");
+
+            if (itens > 20)
+                System.out.println("Aviso: muitos itens");
+
+            System.out.printf("Total final: R$ %.2f\n", total);
+            break;
+
+        default:
+            System.out.println("Opcao invalida");
         }
-        
+
         ler.close();
     }
 }
+
